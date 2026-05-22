8d661f41e4d5226015caccd3238fe630a0111753
Sreekar623 <gudipudisreekar123@gmail.com>
2026-05-22 19:56:21 +0530

    Update HelloController.java

--- RESTAPI/src/main/java/com/sreekar/RESTAPI/controller/HelloController.java
+++ RESTAPI/src/main/java/com/sreekar/RESTAPI/controller/HelloController.java
@@ -1,12 +1,13 @@
 package com.sreekar.RESTAPI.controller;
 
-
 import java.util.*;
 
 import com.sreekar.RESTAPI.entities.Department;
 import com.sreekar.RESTAPI.services.Departmentservice;
-import org.slf4j.LoggerFactory;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -14,49 +15,43 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/departments")
 public class HelloController {
+
     @Autowired
     private Departmentservice ds;
 
+    private static final Logger lg =
+            LoggerFactory.getLogger(HelloController.class);
 
-    private static final Logger lg= LoggerFactory.getLogger(HelloController.class);
+    @Value("${welmsg}")
+    String msg;
 
-@Value("${welmsg}")
-String msg;
-
- @GetMapping()
- public String utthhh(){
-      return msg;
- }
-
+    @GetMapping()
+    public String verifyValue() {
+        return msg;
+    }
 
     @PostMapping()
-    public Department puts(@RequestBody Department d) {
+    public Department saveDepartment(@RequestBody Department d) {
         return ds.savedept(d);
     }
-    @GetMapping("/sohana/details")
-    public String getss(){
-        return "SHE IS GAY";
+
+    @GetMapping("/{id}")
+    public Department getById(@PathVariable("id") int idd) {
+        return ds.getbyid(idd);
     }
-        @GetMapping("/{id}")
-        public Department getss(@PathVariable("id") int idd){
 
-            return ds.getbyid(idd);
-        }
-        @PutMapping("/{id}")
-         public Department updatedept(@PathVariable int id, @RequestBody Department d){
-              return ds.updatedept(id,d);
+    @PutMapping("/{id}")
+    public Department updateDeptById(
+            @PathVariable int id,
+            @RequestBody Department d
+    ) {
+        return ds.updatedept(id, d);
+    }
 
-            }
-
-    @GetMapping("name/{name}")
-    public Department getss(@PathVariable("name") String idd){
-
+    @GetMapping("/name/{name}")
+    public Department getDepartmentByName(
+            @PathVariable("name") String idd
+    ) {
         return ds.getbyname(idd);
     }
-
-
-
-
-
-
 }
